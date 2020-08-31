af38eeb75de4b0ddb6ab10e47e7e026b41b9f44a
Sanny Do <sanny.do94@gmail.com>
2020-08-31 05:19:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,7 @@ end
 #new code below
 def get_japanese_emoticon(path, emoticon)
   load_library(path).each do |key, value|
-    if value[:english] == emoticon
+    if value[:english]
       return key
     end
   end
